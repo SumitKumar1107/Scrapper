d74c8fa03eba0d1d5a081b3a72a851200f39a08e
sumit <nolankumar9690@gmail.com>
2026-02-15 18:11:15 +0530

    deploy

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    reload = os.environ.get("ENV", "dev") != "production"
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=port,
+        reload=reload
     )
